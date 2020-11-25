7dd4b948a067f3e98de93adc757a82d7b08c9fa0
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-11-25 12:57:04 -0500

    Fund-The-Change2.0

--- FundTheChange/src/Components/Authentication/Signup/Signup.js
+++ FundTheChange/src/Components/Authentication/Signup/Signup.js
@@ -4,11 +4,8 @@ import './Signup.css'
 const Signup = () => {
 
   const [contactInfo, setContactInfo] = useState({
-    firstName: '',
-    lastName: '',
     email: '',
-    password: '',
-    phone: ''
+    password: ''
   })
   // handleContactChange is responsible for accessing the value that the user types into the inputs
   const handleContactChange = (e) => {
@@ -32,20 +29,25 @@ const Signup = () => {
   //     },
   //     body: JSON.stringify(contactInfo)
   //   })
-
+  //
   // }
   return (
-    <>
-      <form method="POST" action='/signup'>
+    <div className="root">
+    <div className="container2">
+    <h1>Sign Up</h1>
+      <p className="lead">
+        Sign Up with your email address.
+      </p>
+      <form className="formGroup" method="POST" action='/signup'>
         {/* in order for react to read my inputs they must be in self closing tags */}
-        <input onChange={handleContactChange} name="firstName" placeholder="First Name" />
-        <input onChange={handleContactChange} name="lastName" placeholder="Last Name" />
-        <input onChange={handleContactChange} name="email" placeholder="Email" />
-        <input onChange={handleContactChange} name="password" placeholder="Password" />
-        <input onChange={handleContactChange} name="phone" placeholder="Phone Number" />
-        <button>Sign me up</button>
+
+      
+        <input className="input"  onChange={handleContactChange} name="email" placeholder="Email" />
+        <input className="input"  onChange={handleContactChange} name="password" placeholder="Password" />
+        <button className="button">Sign me up</button>
       </form>
-    </>
+      </div>
+    </div>
   )
 }
 

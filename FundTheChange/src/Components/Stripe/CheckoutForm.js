35d6caaeca7f1b4eac1338ef480bbd8d4409cf35
johnbelmdev <johnbel458@gmail.com>
2020-11-25 14:29:32 -0500

    added components and stripe

---
+++ FundTheChange/src/Components/Stripe/CheckoutForm.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import axios from "axios";
+
+export const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+
+    if (!error) {
+      console.log("Stripe 23 | token generated!", paymentMethod);
+      try {
+        const { id } = paymentMethod;
+        const response = await axios.post(
+          "http://localhost:8000/stripe/charge",
+          {
+            amount: 10099,
+            id: id,
+          }
+        );
+
+        console.log("Stripe 35 | data", response.data.success);
+        if (response.data.success) {
+          console.log("CheckoutForm.js 25 | payment successful!");
+        }
+      } catch (error) {
+        console.log("CheckoutForm.js 28 | ", error);
+      }
+    } else {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
+      <CardElement />
+      <button>Pay</button>
+    </form>
+  );
+};

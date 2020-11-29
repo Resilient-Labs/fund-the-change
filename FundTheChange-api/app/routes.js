192f8d6736c92babb82ed121ece85326a08efe86
kathyle-dev <69046536+kathyle-dev@users.noreply.github.com>
2020-11-29 14:58:32 -0500

    Latest Working APP --- stripe, orgs, login

--- FundTheChange-api/app/routes.js
+++ FundTheChange-api/app/routes.js
@@ -1,18 +1,12 @@
+
+const stripe = require('stripe')('sk_test_51Ha6EUEsCFPlCMG1zxzWoFCMCkMdTEEnxmtsY54cDJ1ZCMebV8NwxX9V9IFrDojB0nhtXTdhk1EVVD1KDiUUPi9g00gVqMcbFl'); // Add your Secret Key Here
+const cors = require("cors");
+
 module.exports = function (app, passport, db, ObjectId) {
 
-  app.get("/userJournals", (req, res) => {
-    let uId = ObjectId(req.session.passport.user);
-    console.log(uId);
+  const fs = require('fs')
 
-    db.collection("journal")
-      .find({ user: uId })
-      .toArray((err, result) => {
-        if (err) return console.log(err);
-
-        console.log(result);
-        res.send({ result: result });
-      });
-  });
+  app.use(cors());
 
   //User Org Search
   app.post("/search", (req, res) => {
@@ -28,14 +22,22 @@ module.exports = function (app, passport, db, ObjectId) {
     }
   });
 
-  const fs = require('fs')
+  //Organization Route - Returns all approved Organizations
+  app.get("/organizations", (req, res) => {
+    db.collection("organizations")
+      .find()
+      .toArray((err, result) => {
+        if (err) return console.log(err);
+        res.send({ result: result });
+      })
+    });
 
-  app.use(cors());
-
-  // process the login form
-  app.post("/login", passport.authenticate("local-login"), (req, res) => {
-    res.redirect("/profile");
-  });
+// process the login form
+app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash profile
+    }));
 
   // process the signup form
   app.post(
@@ -47,80 +49,48 @@ module.exports = function (app, passport, db, ObjectId) {
     })
   );
 
-  app.get("/organizations", (req, res) => {
-    db.collection("organizations")
-      .find()
-      .toArray((err, result) => {
-        if (err) return console.log(err);
-        res.send({ result: result });
-      })
-    };
-    
-  app.post("/stripe/charge/", cors(), async (req, res,) => {
-    // console.log("stripe-routes.js 9 | route reached", req.body);
-    let { amount,customer,id } = req.body;
-// console.log("hello I am db",db);
-    console.log("hell amout",amount);
-      console.log("hello I am organizations id on the route",req.body.organizationId);
 
-    db.collection('donation').save({
-      // setting property for a specific users
+    app.post("/stripe/charge/", cors(), async (req, res,) => {
+      // console.log("stripe-routes.js 9 | route reached", req.body);
+      let { amount,customer,id } = req.body;
+  // console.log("hello I am db",db);
+      console.log("hell amout",amount);
+        console.log("hello I am organizations id on the route",req.body.organizationId);
 
-      // user: req.user,
-
-      organizationId: ObjectId(req.body.organizationId),
-      amount: amount,
-      customer:customer
-    }),
-
-
-
-
-        // db.collection('organizations').save({
-        //   // setting property for a specific users
-        //
-        //   // user: req.user,
-        //   // userId: ObjectId(req.session.passport.email),
-        //
-        //   // organizationId: ObjectId(req.body.organizationId),
-        //   amount: amount,
-        //   userId: ObjectId(req.body.userId),
-        //
-        //   organizationId: ObjectId(req.body.organizationId),
-        //   customer:customer
-        // }),
-
-        // db.organizations.updateOne(
-        //    {   _id: ObjectId("5fbabe871edac7fe934acfd6") },
-        //    {
-        //      $set: { "size.uom": "cm", status: "P" },
-        //      $currentDate: { lastModified: true }
-        //    }
-        // )
-
-      //   db.collection('organizations').update({
-      //     _id: ObjectId("5fbabe871edac7fe934acfd6")
-      // }, {
-      //   "citiName": "Jakarta Selatan",
-      //   "provName": "test update Jakarta",
-      //   amount:amount
-      // }, {
-      //   upsert: true
-      // }),
-    console.log("hell amout",amount);
-
-    // console.log("stripe-routes.js 10 | amount and id", amount, id);
-    try {
-      const payment = await stripe.paymentIntents.create({
+      db.collection('donation').save({
+        // setting property for a specific users
+        organizationId: ObjectId(req.body.organizationId),
         amount: amount,
-        currency: "USD",
-        description: customer,
-        payment_method: id,
-        confirm: true,
-      });
-  });
+        customer:customer
+      }),
 
-  app.get("/users", isLoggedIn, (req, res) => {
+      console.log("hell amout",amount);
+
+      // console.log("stripe-routes.js 10 | amount and id", amount, id);
+      try {
+        const payment = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "USD",
+          description: customer,
+          payment_method: id,
+          confirm: true,
+        });
+        console.log("stripe-routes.js 19 | payment", payment);
+        res.json({
+          message: "Payment Successful",
+          success: true,
+        });
+      } catch (error) {
+        console.log("stripe-routes.js 17 | error", error);
+        res.json({
+          message: "Payment Failed",
+          success: false,
+        });
+      }
+    });
+
+
+  app.get("/users", (req, res) => {
     const uId = ObjectId(req.session.passport.user);
     db.collection("users")
       .find({ _id: uId })
@@ -136,12 +106,11 @@ module.exports = function (app, passport, db, ObjectId) {
   });
 
   app.get("/donationAmount",(req, res) => {
-    console.log("hello I am donationAmount",req.session.passport.user.email);
+    // console.log("hello I am donationAmount",req.session.passport.user.email);
     db.collection("donation")
-      .find({customer:req.session.user.email})
+      .find({})
       .toArray((err, result) => {
         if (err) return console.log(err);
-
         res.send({ result: result });
       });
   });
@@ -159,16 +128,4 @@ module.exports = function (app, passport, db, ObjectId) {
     res.redirect("/");
   });
 
-  // post method to store mood journal entry document to mongodb
-  app.post("/saveJournalEntry", (req, res, next) => {
-    let uId = ObjectId(req.session.passport.user);
-    console.log(uId);
-    db.collection("journal").save(
-      { journal: req.body.journalEntry, mood: req.body.mood, user: uId },
-      (err, result) => {
-        if (err) return console.log(err);
-        res.send({ success: "success" });
-      }
-    );
-  });
 };
